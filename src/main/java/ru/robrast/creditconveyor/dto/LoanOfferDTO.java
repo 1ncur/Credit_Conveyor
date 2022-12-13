bc81fce468b21f5f3bbf60e1899a327fd4111aa3
Robert <116336209+1ncur@users.noreply.github.com>
2022-12-13 22:08:45 +0300

    Add files via upload

---
+++ src/main/java/ru/robrast/creditconveyor/dto/LoanOfferDTO.java
@@ -0,0 +1,18 @@
+package ru.robrast.creditconveyor.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class LoanOfferDTO {
+    private Long applicationId;
+    private BigDecimal requestedAmount;
+    private BigDecimal totalAmount;
+    private Integer term;
+    private BigDecimal monthlyPayment;
+    private BigDecimal rate;
+    private Boolean isInsuranceEnabled;
+    private Boolean isSalaryClient;
+
+}
